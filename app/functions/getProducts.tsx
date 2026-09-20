cd1022f1d61cc014c1f24eec25ab47d6fcdcceba
Marwin Jade <jcardinez03>
2026-09-20 10:11:23 +0800

    updated fetch

--- app/functions/getProducts.tsx
+++ app/functions/getProducts.tsx
@@ -1,5 +1,5 @@
 export const getProducts = async () => {
-    const response = await fetch('http://localhost:8000/api/products', {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {
         method:"GET",
         headers: {
             "Accept":"application/json"
